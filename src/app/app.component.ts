145c330399bd7e79e1e0de32eca6286e7e1221f7
Peter Shaker <pss@d60.dk>
2019-03-21 12:20:36 +0200

    POC Drag and Drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = "angular-peter";
+  orderableLists = [
+    ["Item 1a", "Item 2a", "Item 3a"],
+    ["Item 1b", "Item 2b", "Item 3b"]
+  ];
 }
